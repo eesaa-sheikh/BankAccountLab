ba3bf311997df13c3185e6c28065c65cb43fdfa7
eesaasheikh <eesaasheikh3@gmail.com>
2022-11-16 01:00:58 +0000

    init commit

--- src/main/java/mainBank.java
+++ src/main/java/mainBank.java
@@ -6,7 +6,7 @@ public class mainBank {
         Date currentDate = new Date();
         System.out.println(currentDate);
 
-        bankAccount eesaa = new bankAccount("eesaa","sheikh", 1234, 01.1999, 100);
+        bankAccount eesaa = new bankAccount("eesaa","sheikh", 1234, 01.1999, 150);
         eesaa.optionMenu();
 
     }
